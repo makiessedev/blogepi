6ab3d012d80dc43627f0cc69de316370a371c307
Meick Mora <meickmora9@gmail.com>
2023-09-15 15:45:32 +0100

    db: seed

---
+++ prisma/seed/post.ts
@@ -0,0 +1,62 @@
+export const posts = [
+  {
+    title: '🎉 Feliz Dia do Programador! 🎉',
+    description:
+      'Hoje celebramos todos os mestres da lógica, os solucionadores de quebra-cabeças e os criadores de códigos que tornam o mundo digital possível. 🖥️💻',
+    content:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor bibendum elit, id rhoncus justo venenatis vel. Integer auctor turpis in sem dictum, nec tristique elit laoreet. Vivamus a ultricies ipsum. Sed non purus eget arcu auctor tincidunt. Phasellus bibendum, justo eu facilisis feugiat, urna justo ultricies metus, a fermentum urna elit eget arcu. Nullam sit amet ultricies ipsum. Sed bibendum enim eget justo rhoncus, in scelerisque libero tincidunt. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Proin euismod varius nulla, nec vulputate metus venenatis sed. Sed auctor tortor eu nibh laoreet volutpat. Sed at vehicula arcu. Vivamus nec diam in massa malesuada vehicula a id arcu',
+    authorUrl: 'https://www.kudikadigital.com',
+    author: 'Dionísio Fama',
+    isPublish: true,
+  },
+  {
+    title: 'Entendendo as Bases: Linguagens de Programação',
+    description:
+      'Começar no mundo da programação pode ser intimidante, mas não se preocupe - estamos aqui para ajudar! Hoje, vamos dar os primeiros passos para entender as diferentes linguagens de programação.',
+    content:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor bibendum elit, id rhoncus justo venenatis vel. Integer auctor turpis in sem dictum, nec tristique elit laoreet. Vivamus a ultricies ipsum. Sed non purus eget arcu auctor tincidunt. Phasellus bibendum, justo eu facilisis feugiat, urna justo ultricies metus, a fermentum urna elit eget arcu. Nullam sit amet ultricies ipsum. Sed bibendum enim eget justo rhoncus, in scelerisque libero tincidunt. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Proin euismod varius nulla, nec vulputate metus venenatis sed. Sed auctor tortor eu nibh laoreet volutpat. Sed at vehicula arcu. Vivamus nec diam in massa malesuada vehicula a id arcu',
+    authorUrl: 'https://www.kudikadigital.com',
+    author: 'Dionísio Fama',
+    isPublish: true,
+  },
+  {
+    title: 'Fique por Dentro: Novidades da Programação',
+    description:
+      'Hoje, estamos lançando nossa jornada para manter você atualizado com as últimas tendências e inovações no mundo da programação. A tecnologia avança rapidamente, e é importante estar à frente da curva',
+    content:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor bibendum elit, id rhoncus justo venenatis vel. Integer auctor turpis in sem dictum, nec tristique elit laoreet. Vivamus a ultricies ipsum. Sed non purus eget arcu auctor tincidunt. Phasellus bibendum, justo eu facilisis feugiat, urna justo ultricies metus, a fermentum urna elit eget arcu. Nullam sit amet ultricies ipsum. Sed bibendum enim eget justo rhoncus, in scelerisque libero tincidunt. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Proin euismod varius nulla, nec vulputate metus venenatis sed. Sed auctor tortor eu nibh laoreet volutpat. Sed at vehicula arcu. Vivamus nec diam in massa malesuada vehicula a id arcu',
+    authorUrl: 'https://www.kudikadigital.com',
+    author: 'Dionísio Fama',
+    isPublish: true,
+  },
+  {
+    title: 'Dica Rápida: Aumentando sua Produtividade de Programação',
+    description:
+      'Ser um programador eficiente é fundamental para o sucesso. Aqui está uma dica rápida para aumentar sua produtividade na codificação:',
+    content:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor bibendum elit, id rhoncus justo venenatis vel. Integer auctor turpis in sem dictum, nec tristique elit laoreet. Vivamus a ultricies ipsum. Sed non purus eget arcu auctor tincidunt. Phasellus bibendum, justo eu facilisis feugiat, urna justo ultricies metus, a fermentum urna elit eget arcu. Nullam sit amet ultricies ipsum. Sed bibendum enim eget justo rhoncus, in scelerisque libero tincidunt. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Proin euismod varius nulla, nec vulputate metus venenatis sed. Sed auctor tortor eu nibh laoreet volutpat. Sed at vehicula arcu. Vivamus nec diam in massa malesuada vehicula a id arcu',
+    authorUrl: 'https://www.kudikadigital.com',
+    author: 'Dionísio Fama',
+    isPublish: true,
+  },
+  {
+    title: 'Explore o Mundo do Open Source 🌐',
+    description:
+      'Hoje, vamos falar sobre o emocionante mundo do código aberto (open source) e como você pode participar!',
+    content:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor bibendum elit, id rhoncus justo venenatis vel. Integer auctor turpis in sem dictum, nec tristique elit laoreet. Vivamus a ultricies ipsum. Sed non purus eget arcu auctor tincidunt. Phasellus bibendum, justo eu facilisis feugiat, urna justo ultricies metus, a fermentum urna elit eget arcu. Nullam sit amet ultricies ipsum. Sed bibendum enim eget justo rhoncus, in scelerisque libero tincidunt. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Proin euismod varius nulla, nec vulputate metus venenatis sed. Sed auctor tortor eu nibh laoreet volutpat. Sed at vehicula arcu. Vivamus nec diam in massa malesuada vehicula a id arcu',
+    authorUrl: 'https://www.kudikadigital.com',
+    author: 'Dionísio Fama',
+    isPublish: true,
+  },
+  {
+    title: 'Desenvolvimento Web: Dicas para uma Melhor Experiência do Usuário',
+    description:
+      'A experiência do usuário (UX) é fundamental para o sucesso de qualquer aplicativo ou site. Hoje, compartilhamos algumas dicas valiosas para melhorar a UX em desenvolvimento web. ✨🌐',
+    content:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor bibendum elit, id rhoncus justo venenatis vel. Integer auctor turpis in sem dictum, nec tristique elit laoreet. Vivamus a ultricies ipsum. Sed non purus eget arcu auctor tincidunt. Phasellus bibendum, justo eu facilisis feugiat, urna justo ultricies metus, a fermentum urna elit eget arcu. Nullam sit amet ultricies ipsum. Sed bibendum enim eget justo rhoncus, in scelerisque libero tincidunt. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Proin euismod varius nulla, nec vulputate metus venenatis sed. Sed auctor tortor eu nibh laoreet volutpat. Sed at vehicula arcu. Vivamus nec diam in massa malesuada vehicula a id arcu',
+    authorUrl: 'https://www.kudikadigital.com',
+    author: 'Dionísio Fama',
+    isPublish: true,
+  },
+];
---
+++ prisma/seed/seed.ts
@@ -0,0 +1,44 @@
+import { PrismaClient } from '@prisma/client';
+import { hashSync } from 'bcrypt';
+
+import { posts } from './post';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const admin = await prisma.user.create({
+    data: {
+      name: 'kudika',
+      email: 'admin@kudika.com',
+      password: hashSync('kudika', 8),
+      isAdmin: true,
+    },
+  });
+
+  const raw = posts.map(
+    async ({ author, authorUrl, content, description, isPublish, title }) => {
+      return await prisma.post.create({
+        data: {
+          userId: admin.id,
+          author,
+          authorUrl,
+          content,
+          description,
+          isPublish,
+          title,
+        },
+      });
+    },
+  );
+
+  console.log({ admin, raw });
+}
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
